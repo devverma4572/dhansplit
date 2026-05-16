af4e6329f7e7aefbd4c78c11ec19be7050dd3083
devverma4572 <lkvmpi@gmail.com>
2026-05-16 15:05:29 +0530

    Name Edit Input

--- src/app/(modals)/Profile-Modal.tsx
+++ src/app/(modals)/Profile-Modal.tsx
@@ -3,10 +3,25 @@ import Header from '@/components/ui/Header';
 import ModalWrapper from '@/components/ui/ModalWrapper';
 import { colors, radius, spacingX, spacingY } from '@/constants/theme';
 import { verticalScale } from "@/utils/styling";
-import React from "react";
-import { StyleSheet, View } from "react-native";
+import { Image } from "expo-image";
+import * as Icons from 'phosphor-react-native';
+import React, { useState } from "react";
+import { Button, ScrollView, StyleSheet, TextInput, TouchableOpacity, View } from "react-native";
+import { auth } from '../../../config/firebase';
+import { UserDataType } from '../../../types/types';
+import Typo from '../../components/ui/Typo';
 
 const ProfileModal = () => {
+  const user = auth.currentUser;
+  const profileImage = user?.photoURL;
+  console.log(profileImage);
+
+  const [userData, setUserData] = useState<UserDataType>({
+    name:"",
+    image: null,
+  })
+    
+
   return (
     <ModalWrapper>
       <View style={styles.container}>
@@ -15,6 +30,58 @@ const ProfileModal = () => {
             leftIcon={<BackButton />}
             style = {{marginBottom: spacingY._10}}
             />
+      
+      {/* ------------------------------FORM-------------------------------- */}
+
+
+
+
+    {/* ---------------------------------------Profile Image------------------------------- */}
+      <ScrollView contentContainerStyle={styles.form}>
+        <View style={styles.avatarContainer}>
+          <Image style={styles.avatar}
+          source={
+            profileImage
+              ? {uri: profileImage}
+              : require('../../../assets/images/defaultAvatar.png')
+          }
+          contentFit="cover"
+          transition={100}    
+          />
+
+          <TouchableOpacity style={styles.editIcon}>
+            <Icons.Pencil
+              size={verticalScale(20)}
+              color={colors.neutral800} 
+            />
+          </TouchableOpacity>
+        </View>
+
+
+    {/* -------------------------------------Name Input ----------------------------------------------------- */}
+        <View style={styles.inputContainer}>
+          <Typo color={colors.neutral200}>Name</Typo>
+          <TextInput style={styles.tinput}
+          placeholder='Name'
+          value={userData.name}
+          onChangeText={(value) => setUserData({...userData, name: value})
+        }
+        />
+        </View>
+        
+        {/* ----------------------------------------------- Footer ---------------------------------------------------------- */}
+        <View style={styles.footer}>
+          <Button onPress={onSubmit} style={{flex: 1}} >
+
+          </Button>
+        </View>
+
+
+
+
+        </ScrollView>
+
+
       </View>
     </ModalWrapper>
   );
@@ -24,7 +91,7 @@ export default ProfileModal;
 
 const styles = StyleSheet.create({
     container:{
-        flex: 0,
+        flex: 1,
         paddingHorizontal: spacingX._10,
         
     },
@@ -37,7 +104,7 @@ const styles = StyleSheet.create({
     avatarContainer:{
       position: "relative",
       alignSelf: "center",
-      marginTop: verticalScale(80),
+      marginTop: spacingY._20,
     },
     avatar:{
       alignSelf: "center",
@@ -45,6 +112,10 @@ const styles = StyleSheet.create({
       height: verticalScale(100),
       width: verticalScale(100),
       borderRadius: 100,
+    },
+    form:{
+      gap: spacingY._30,
+      marginTop: spacingY._15,
     },
     editIcon:{
       position: "absolute",
@@ -94,4 +165,34 @@ const styles = StyleSheet.create({
       width: "100%",
       paddingVertical: verticalScale(10),
     },
-});
+    inputContainer:{
+      gap: spacingY._15,
+    },
+    tinput:{
+      height: verticalScale(55),
+      backgroundColor: colors.neutral800,
+      borderRadius: radius._15,
+      paddingHorizontal: spacingX._15,
+      fontSize: verticalScale(15),
+      color: colors.white,
+      borderWidth: 1,
+      borderColor: colors.neutral700,
+
+      shadowColor: colors.black,
+      shadowOffset: {
+        width: 0,
+        height: 4,
+      }
+    },
+    footer:{
+      alignItems: "center",
+      flexDirection: "row",
+      justifyContent: "center",
+      paddingHorizontal: spacingX._20,
+      gap: scale(12),
+      paddingTop: spacingY._15,
+      borderTopColor: colors.neutral700,
+      marginBottom: spacingY._5,
+      borderTopWidth: 1,
+    }
+  });
